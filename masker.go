645ff4cccad30493c0ed20bc8388d8b0d2fe7e49
njrogie <43356043+njrogie@users.noreply.github.com>
2022-07-17 21:58:09 -0500

    Added first unit-test

--- masker.go
+++ masker.go
@@ -1,10 +1,36 @@
 package imagemasker
 
-import "os"
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+// reusable func for getting an image from a .png file
+func getImage(filename string) image.Image {
+	reader, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("Failed to open file: ", err)
+	}
+	defer reader.Close()
+
+	img, err := png.Decode(reader)
+	if err != nil {
+		log.Fatal("Failed to decode image ", filename, err)
+	}
+
+	return img
+}
+
+func findAbsoluteMatch(origImage string, maskImage string) {
+	fmt.Println(getImage(origImage).Bounds().Max)
+	fmt.Println(getImage(maskImage).Bounds().Max)
+}
 
 func main() {
-	subjectFile := os.Args[1]
-	maskFile := os.Args[2]
-
+	//subjectFile := os.Args[1]
+	//maskFile := os.Args[2]
 
 }
